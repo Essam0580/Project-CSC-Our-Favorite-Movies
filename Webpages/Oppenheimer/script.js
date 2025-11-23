f5cec1b5e4a3838b094a81831c2f7519909a4000
wedhamza41 <hamzaehabismail@gmail.com>
2025-11-23 23:10:09 +0200

    Update script.js

--- Webpages/Oppenheimer/script.js
+++ Webpages/Oppenheimer/script.js
@@ -1,5 +1,7 @@
 function toggleDropdown() {
     const list = document.getElementById("websiteList");
+    
+    // Check if it is currently visible
     if (list.style.display === "block") {
         list.style.display = "none";
     } else {
